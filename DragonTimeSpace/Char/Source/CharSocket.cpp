88d48b532e8127fa597cd1ce50bd439390d4ea72
AKCore <dblegendsmud@gmail.com>
2020-03-16 00:53:46 -0700

    add team protobufpackets, add response to team shit.

--- DragonTimeSpace/Char/Source/CharSocket.cpp
+++ DragonTimeSpace/Char/Source/CharSocket.cpp
@@ -7,6 +7,7 @@
 #include <Network\Messages\avatar.pb.h>
 #include <Network\Messages\object.pb.h>
 #include <Network\Messages\hero.pb.h>
+#include <Network\Messages\team.pb.h>
 //#include <Utils/Opcodes.h>
 //#include <Utils/ResultCode.h>
 #include <Utils/Logger/Logger.h>
@@ -30,6 +31,7 @@ CharSocket::CharSocket(boost::asio::io_context &service)
 	methodList.emplace_back(CommandID::SetMainHero_CSC, std::bind(&CharSocket::onReceiveMainHero, this, std::placeholders::_1));
 	methodList.emplace_back(CommandID::Notify_SceneLoaded_CS, std::bind(&CharSocket::onSceneLoaded, this, std::placeholders::_1));
 	methodList.emplace_back(CommandID::Create_Role_CS, std::bind(&CharSocket::onReceiveCharCreate, this, std::placeholders::_1));
+	methodList.emplace_back(CommandID::TeamMemeberList_CS, std::bind(&CharSocket::onReceiveTeamMemberReq, this, std::placeholders::_1));
 
 	// debug test
 	//methodList.emplace_back(2273, std::bind(&CharSocket::onReceiveProtobuf, this, std::placeholders::_1));
@@ -134,7 +136,7 @@ bool CharSocket::onReceiveUserInfo(const Packet& packet)
 		it->set_hairstyle(45);
 		it->set_avatarid(4001);
 		it->set_mapname(std::move(std::string("Time and space city")));
-		it->set_name(std::move(std::string("TaSLPServer")));
+		it->set_name(std::move(std::string("Sangawku")));
 
 	const unsigned short protobuff_data_size = characterList->ByteSizeLong();
 	std::vector<BYTE> protobuff_buffer(protobuff_data_size);
@@ -434,11 +436,11 @@ bool CharSocket::onReceiveMainHero(const Packet& packet)
 	LOG_DEBUG << "on main hero request received";
 	hero::MSG_SetMainHero_CSC _hero;
 	fill_my_data(_hero, (unsigned char*)packet.GetPacketData(), packet.GetPacketHeader().size);
-
+	LOG_DEBUG << _hero.DebugString();
 	hero::MSG_SetMainHero_CSC * hero = new hero::MSG_SetMainHero_CSC();
 	hero->set_errorcode(0);
 	hero->set_opcode(1);
-	hero->set_herothisid(70022);
+	hero->set_herothisid(_hero.herothisid());
 
 	const unsigned short protobuff_data_size = hero->ByteSizeLong();
 	std::vector<BYTE> protobuff_buffer(protobuff_data_size);
@@ -467,12 +469,152 @@ bool CharSocket::onReceiveMainHero(const Packet& packet)
 		buffer.Write(&++padding, 1);
 
 	_writeQueue.push(std::move(buffer));;
+
+
 	return true;
 }
 
 bool CharSocket::onSceneLoaded(const Packet& packet)
 {
 	LOG_DEBUG << "on scene loaded request received";
+	return true;
+}
+
+bool CharSocket::onReceiveTeamMemberReq(const Packet& packet)
+{
+	Team::MSG_TeamMemeberList_CS _team;
+	fill_my_data(_team, (unsigned char*)packet.GetPacketData(), packet.GetPacketHeader().size);
+	LOG_DEBUG << _team.DebugString();
+	Team::MSG_TeamMemeberList_SC* team = new Team::MSG_TeamMemeberList_SC();
+	team->set_id(70022);
+	team->set_curmember(0);
+	
+
+	const unsigned short protobuff_data_size = team->ByteSizeLong();
+	std::vector<BYTE> protobuff_buffer(protobuff_data_size);
+	team->SerializeToArray(protobuff_buffer.data(), protobuff_data_size);
+
+	res_test resp;
+	MessageBuffer buffer;
+
+	buffer.Resize((sizeof(res_test) + protobuff_data_size));
+
+	resp.size = (sizeof(res_test) - HEADER_SIZE) + protobuff_data_size;
+	resp.CMD = 60110;
+	resp.compress = 0;
+	resp.encrypt = 0;
+	resp.pad = 0x81;
+	resp.pad1 = 0xde;
+	resp.pad2 = 0x46;
+	resp.pad3 = 0xdf;
+	resp.protobuff_length = protobuff_data_size;
+
+	buffer.Write(&resp, sizeof(res_test));
+	buffer.Write(protobuff_buffer.data(), protobuff_data_size);
+	// -- padding
+	BYTE padding = 0;
+	while (((sizeof(res_test) + protobuff_data_size) + padding) % 8 != 0 && padding < 10)
+		buffer.Write(&++padding, 1);
+
+	_writeQueue.push(std::move(buffer));;
+
+
+
+	//Main Data
+	msg::MSG_DataCharacterMain_SC* pktMain = new msg::MSG_DataCharacterMain_SC();
+	msg::CharacterMainData* charMain = new msg::CharacterMainData();
+	msg::AttributeData* attrData = new msg::AttributeData();
+	msg::CharacterBaseData* charBase = new msg::CharacterBaseData();
+	msg::CharacterFightData* fightBase = new msg::CharacterFightData();
+	msg::MapUserData* mapBase = new msg::MapUserData;
+	msg::CharacterMapData* mapdata = new msg::CharacterMapData;
+	msg::CharacterMapShow* mapshow = new msg::CharacterMapShow;
+	attrData->set_accurate(100);
+	attrData->set_bang(100);
+	attrData->set_block(100);
+	attrData->set_maxhp(100);
+	attrData->set_maxmp(100);
+	attrData->set_hp(100);
+	attrData->set_mp(100);
+	
+
+	charBase->set_exp(10000);
+	charBase->set_cooppoint(1000);
+	std::string s("Female Saiyan");
+	charBase->set_herothisid(s);
+
+	charBase->set_viplevel(5);
+	mapdata->set_hp(100);
+	mapdata->set_maxhp(100);
+	mapdata->set_level(10);
+	
+	msg::FloatMovePos pos;
+	pos.set_fx(801.0f);
+	pos.set_fy(905.0f);
+
+	mapdata->set_allocated_pos(&pos);
+	mapdata->set_movespeed(30);
+	mapdata->set_country(1);
+	mapdata->set_dir(1);
+	mapshow->set_heroid(70022);
+	mapshow->set_avatarid(70022);
+	mapshow->set_occupation(1);
+	mapshow->set_hairstyle(43);
+	mapshow->set_haircolor(86);
+	mapshow->set_facestyle(114);
+
+	mapBase->set_allocated_mapdata(mapdata);
+	mapBase->set_allocated_mapshow(mapshow);
+	mapBase->set_charid(70022);
+	mapBase->set_name(std::string("SanGawku"));
+	fightBase->set_curfightvalue(0);
+	charBase->set_type(2);
+
+
+	charMain->set_allocated_attridata(attrData);
+	charMain->set_allocated_basedata(charBase);
+	charMain->set_allocated_fightdata(fightBase);
+	charMain->set_allocated_mapdata(mapBase);
+	pktMain->set_allocated_data(charMain);
+	//cutscene->set_onfinish();
+
+	const unsigned short protobuff_data_size3 = pktMain->ByteSizeLong();
+	std::vector<BYTE> protobuff_buffer3(protobuff_data_size3);
+	pktMain->SerializeToArray(protobuff_buffer3.data(), protobuff_data_size3);
+
+
+	LOG_DEBUG << "Protobuff packet to send HEX: ";
+	log_data(protobuff_buffer3.data(), protobuff_data_size3);
+
+	res_test resp3;
+	MessageBuffer buffer3;
+
+	buffer3.Resize((sizeof(res_test) + protobuff_data_size3));
+
+	resp3.size = (sizeof(res_test) - HEADER_SIZE) + protobuff_data_size3;
+	resp3.CMD = 2261;
+	resp3.compress = 0;
+	resp3.encrypt = 0;
+	resp3.pad = 0x81;//Timestamp
+	resp3.pad1 = 0xde;
+	resp3.pad2 = 0x46;
+	resp3.pad3 = 0xdf;
+	resp3.protobuff_length = protobuff_data_size3;
+
+
+	buffer3.Write(&resp3, sizeof(res_test));
+	buffer3.Write(protobuff_buffer3.data(), protobuff_data_size3);
+	LOG_DEBUG << "Before Padding Size " << buffer3.GetBufferSize();
+	// -- padding
+	padding = 0;
+	while (((sizeof(res_test) + protobuff_data_size3) + padding) % 8 != 0 && padding < 10)
+		buffer3.Write(&++padding, 1);
+
+
+	log_data(buffer3.GetReadPointer(), sizeof(res_test) + protobuff_data_size3 + 2);
+	LOG_DEBUG << "Real Packet Size: " << buffer3.GetBufferSize();
+
+	_writeQueue.push(std::move(buffer3));
 	return true;
 }
 
