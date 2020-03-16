f510213cca59a5eb21939ebd80cf7ba49ac21434
AKCore <dblegendsmud@gmail.com>
2020-03-16 03:05:35 -0700

    Can now login to game!! FUCK YEAH SUCK OUR D. Still need to fix the Char Create to put you in game with new character.

--- DragonTimeSpace/Char/Source/CharSocket.cpp
+++ DragonTimeSpace/Char/Source/CharSocket.cpp
@@ -32,6 +32,7 @@ CharSocket::CharSocket(boost::asio::io_context &service)
 	methodList.emplace_back(CommandID::Notify_SceneLoaded_CS, std::bind(&CharSocket::onSceneLoaded, this, std::placeholders::_1));
 	methodList.emplace_back(CommandID::Create_Role_CS, std::bind(&CharSocket::onReceiveCharCreate, this, std::placeholders::_1));
 	methodList.emplace_back(CommandID::TeamMemeberList_CS, std::bind(&CharSocket::onReceiveTeamMemberReq, this, std::placeholders::_1));
+	methodList.emplace_back(CommandID::Req_SelectCharToLogin_CS, std::bind(&CharSocket::onSelectCharToLogin, this, std::placeholders::_1));
 
 	// debug test
 	//methodList.emplace_back(2273, std::bind(&CharSocket::onReceiveProtobuf, this, std::placeholders::_1));
@@ -240,15 +241,15 @@ bool CharSocket::onReceiveCharCreate(const Packet& packet)
 	fill_my_data(_hero, (unsigned char*)packet.GetPacketData(), packet.GetPacketHeader().size);
 
 	msg::MSG_Ret_UserMapInfo_SC* mapInfo = new msg::MSG_Ret_UserMapInfo_SC();
-	msg::FloatMovePos pos;
-	pos.set_fx(801.f);
-	pos.set_fy(908.f);
+	msg::FloatMovePos *pos = new msg::FloatMovePos();
+	pos->set_fx(801.f);
+	pos->set_fy(908.f);
 	mapInfo->set_mapid(695);
 	mapInfo->set_filename(std::move(std::string("Pc_sgc")));
 	mapInfo->set_mapname(std::move(std::string("Time and space city")));
 	mapInfo->set_lineid(0);
 	mapInfo->set_sceneid(0);
-	mapInfo->set_allocated_pos(&pos);
+	mapInfo->set_allocated_pos(pos);
 	mapInfo->set_copymapidx(0);
 	mapInfo->set_subcopymapidx(0);
 
@@ -375,58 +376,171 @@ bool CharSocket::onReceiveCharCreate(const Packet& packet)
 	_writeQueue.push(std::move(buffer2));
 
 
-	/*
-	LOG_DEBUG << "Received user info";
-	stIphoneLoginUserCmd_CS* data = (stIphoneLoginUserCmd_CS*)packet.GetPacketData();
-	msg::MSG_Ret_UserMapInfo_SC* mapInfo = new msg::MSG_Ret_UserMapInfo_SC();
-	msg::FloatMovePos pos;
+	//Set Main Char
+	msg::MSG_DataCharacterMain_SC* pktMain = new msg::MSG_DataCharacterMain_SC();
+	msg::CharacterMainData* charMain = new msg::CharacterMainData();
+	msg::AttributeData* attrData = new msg::AttributeData();
+	msg::CharacterBaseData* charBase = new msg::CharacterBaseData();
+	msg::CharacterFightData* fightBase = new msg::CharacterFightData();
+	msg::MapUserData* mapBase = new msg::MapUserData;
+	msg::CharacterMapData* mapdata = new msg::CharacterMapData;
+	msg::CharacterMapShow* mapshow = new msg::CharacterMapShow;
+	attrData->set_hp(10);
+	attrData->set_maxhp(10);
+	attrData->set_mp(10);
+	attrData->set_maxmp(10);
+	attrData->set_str(10);
+	attrData->set_dex(10);
+	attrData->set_intel(10);
+	attrData->set_phy(10);
+	attrData->set_matt(10);
+	attrData->set_patt(10);
+	attrData->set_mdef(10);
+	attrData->set_pdef(10);
+	attrData->set_bang(10);
+	attrData->set_bangextradamage(10);
+	attrData->set_toughness(10);
+	attrData->set_toughnessreducedamage(10);
+	attrData->set_penetrate(10);
+	attrData->set_hit(10);
+	attrData->set_penetrateextradamage(10);
+	attrData->set_block(10);
+	attrData->set_blockreducedamage(10);
+	attrData->set_accurate(10);
+	attrData->set_accurateextradamage(10);
+	attrData->set_hold(10);
+	attrData->set_holdreducedamage(10);
+	attrData->set_deflect(10);
+	attrData->set_deflectreducedamage(10);
+	attrData->set_dodge(10);
+	attrData->set_weaponatt(10);
+	attrData->set_firemastery(10);
+	attrData->set_icemastery(10);
+	attrData->set_lightningmastery(10);
+	attrData->set_brightmastery(10);
+	attrData->set_darkmastery(10);
+	attrData->set_fireresist(10);
+	attrData->set_iceresist(10);
+	attrData->set_lightningresist(10);
+	attrData->set_brightresist(10);
+	attrData->set_darkresist(10);
+	attrData->set_firepen(10);
+	attrData->set_icepen(10);
+	attrData->set_lightningpen(10);
+	attrData->set_brightpen(10);
+	attrData->set_darkpen(10);
+	attrData->set_blowint(10);
+	attrData->set_knockint(10);
+	attrData->set_floatint(10);
+	attrData->set_superhitint(10);
+	attrData->set_blowresist(10);
+	attrData->set_knockresist(10);
+	attrData->set_floatresist(10);
+	attrData->set_superhitresist(10);
+	attrData->set_blowdectime(10);
+	attrData->set_knockdectime(10);
+	attrData->set_floatdectime(10);
+	attrData->set_superhitdectime(10);
+	attrData->set_stiffaddtime(10);
+	attrData->set_stiffdectime(10);
 
-	mapInfo->set_mapid(698);
-	mapInfo->set_filename(std::move(std::string("toto")));
-	mapInfo->set_mapname(std::move(std::string("toto")));
-	mapInfo->set_lineid(1);
-	mapInfo->set_sceneid(1);
-	mapInfo->set_allocated_pos(&pos);
-	mapInfo->set_copymapidx(0);
-	mapInfo->set_subcopymapidx(0);
 
-	const unsigned short protobuff_data_size = mapInfo->ByteSizeLong();
-	std::vector<BYTE> protobuff_buffer(protobuff_data_size);
-	mapInfo->SerializeToArray(protobuff_buffer.data(), protobuff_data_size);
+	charBase->set_exp(1);
+	charBase->set_welpoint(1);
+	charBase->set_money(1);
+	charBase->set_stone(1);
+	charBase->set_tilizhi(1);
+	charBase->set_type(1);
+	charBase->set_famelevel(1);
+	charBase->set_position(0);
+	charBase->set_viplevel(1);
+	charBase->set_port(0);
+	charBase->set_laststage(1);
+	charBase->set_nextexp(1000);
+	charBase->set_pkmode(0);
+	charBase->set_edupoint(1);
+	charBase->set_cooppoint(1);
+	charBase->set_bluecrystal(1);
+	charBase->set_purplecrystal(1);
+	charBase->set_vigourpoint(1);
+	charBase->set_doublepoint(1);
+	charBase->set_bluecrystalincnum(1);
+	charBase->set_purplecrystalincnum(1);
+	charBase->set_familyatt(0);
+	charBase->set_herothisid((std::string)"70022");
+
+	mapdata->set_hp(100);
+	mapdata->set_maxhp(100);
+	mapdata->set_level(10);
+
+
+	mapdata->set_allocated_pos(pos);
+	mapdata->set_movespeed(10);
+	mapdata->set_country(1);
+	mapshow->set_weapon(0);
+
+	mapdata->set_dir(0);
+	mapshow->set_heroid(70022);
+	mapshow->set_avatarid(70022);
+	mapshow->set_occupation(0);
+	mapshow->set_hairstyle(43);
+	mapshow->set_haircolor(86);
+	mapshow->set_facestyle(49);
+	mapshow->set_bodystyle(0);
+	mapBase->set_charid(70022);
+	mapBase->set_name(std::string("SanGawku"));
+
+	mapBase->set_allocated_mapdata(mapdata);
+	mapBase->set_allocated_mapshow(mapshow);
+	mapBase->set_allocated_bakhero(mapshow);
+
+	fightBase->set_curfightvalue(0);
+
+
+	charMain->set_allocated_attridata(attrData);
+	charMain->set_allocated_basedata(charBase);
+	charMain->set_allocated_fightdata(fightBase);
+	charMain->set_allocated_mapdata(mapBase);
+	pktMain->set_allocated_data(charMain);
+	//cutscene->set_onfinish();
+
+	const unsigned short protobuff_data_size3 = pktMain->ByteSizeLong();
+	std::vector<BYTE> protobuff_buffer3(protobuff_data_size3);
+	pktMain->SerializeToArray(protobuff_buffer3.data(), protobuff_data_size3);
 
 
 	LOG_DEBUG << "Protobuff packet to send HEX: ";
-	log_data(protobuff_buffer.data(), protobuff_data_size);
+	log_data(protobuff_buffer3.data(), protobuff_data_size3);
 
-	res_test resp;
-	MessageBuffer buffer;
+	res_test resp3;
+	MessageBuffer buffer3;
 
-	buffer.Resize((sizeof(res_test) + protobuff_data_size));
+	buffer3.Resize((sizeof(res_test) + protobuff_data_size3));
 
-	resp.size = (sizeof(res_test) - HEADER_SIZE) + protobuff_data_size;
-	resp.CMD = 2273;
-	resp.compress = 0;
-	resp.encrypt = 0;
-	resp.pad = 0x81;
-	resp.pad1 = 0xde;
-	resp.pad2 = 0x46;
-	resp.pad3 = 0xdf;
-	resp.protobuff_length = protobuff_data_size;
+	resp3.size = (sizeof(res_test) - HEADER_SIZE) + protobuff_data_size3;
+	resp3.CMD = 2261;
+	resp3.compress = 0;
+	resp3.encrypt = 0;
+	resp3.pad = 0x81;//Timestamp
+	resp3.pad1 = 0xde;
+	resp3.pad2 = 0x46;
+	resp3.pad3 = 0xdf;
+	resp3.protobuff_length = protobuff_data_size3;
 
 
-	buffer.Write(&resp, sizeof(res_test));
-	buffer.Write(protobuff_buffer.data(), protobuff_data_size);
-	LOG_DEBUG << buffer.GetBufferSize();
+	buffer3.Write(&resp3, sizeof(res_test));
+	buffer3.Write(protobuff_buffer3.data(), protobuff_data_size3);
+	LOG_DEBUG << "Before Padding Size " << buffer3.GetBufferSize();
 	// -- padding
-	BYTE padding = 0;
-	while (((sizeof(res_test) + protobuff_data_size) + padding) % 8 != 0 && padding < 10)
-		buffer.Write(&++padding, 1);
+	padding = 0;
+	while (((sizeof(res_test) + protobuff_data_size3) + padding) % 8 != 0 && padding < 10)
+		buffer3.Write(&++padding, 1);
 
-	LOG_DEBUG << "PACKET TO SEND HEX:";
-	log_data(buffer.GetReadPointer(), sizeof(res_test) + protobuff_data_size);
 
-	_writeQueue.push(std::move(buffer));
-	*/
+	log_data(buffer3.GetReadPointer(), sizeof(res_test) + protobuff_data_size3 + 2);
+	LOG_DEBUG << "Real Packet Size: " << buffer3.GetBufferSize();
+
+	_writeQueue.push(std::move(buffer3));
 	return true;
 }
 
@@ -521,6 +635,166 @@ bool CharSocket::onReceiveTeamMemberReq(const Packet& packet)
 
 
 	//Main Data
+	//msg::MSG_DataCharacterMain_SC* pktMain = new msg::MSG_DataCharacterMain_SC();
+	//msg::CharacterMainData* charMain = new msg::CharacterMainData();
+	//msg::AttributeData* attrData = new msg::AttributeData();
+	//msg::CharacterBaseData* charBase = new msg::CharacterBaseData();
+	//msg::CharacterFightData* fightBase = new msg::CharacterFightData();
+	//msg::MapUserData* mapBase = new msg::MapUserData;
+	//msg::CharacterMapData* mapdata = new msg::CharacterMapData;
+	//msg::CharacterMapShow* mapshow = new msg::CharacterMapShow;
+	//attrData->set_accurate(100);
+	//attrData->set_bang(100);
+	//attrData->set_block(100);
+	//attrData->set_maxhp(100);
+	//attrData->set_maxmp(100);
+	//attrData->set_hp(100);
+	//attrData->set_mp(100);
+	//
+
+	//charBase->set_exp(10000);
+	//charBase->set_cooppoint(1000);
+	//std::string s("Female Saiyan");
+	//charBase->set_herothisid(s);
+
+	//charBase->set_viplevel(5);
+	//mapdata->set_hp(100);
+	//mapdata->set_maxhp(100);
+	//mapdata->set_level(10);
+	//
+	//msg::FloatMovePos pos;
+	//pos.set_fx(801.0f);
+	//pos.set_fy(905.0f);
+
+	//mapdata->set_allocated_pos(&pos);
+	//mapdata->set_movespeed(30);
+	//mapdata->set_country(1);
+	//mapdata->set_dir(1);
+	//mapshow->set_heroid(70022);
+	//mapshow->set_avatarid(70022);
+	//mapshow->set_occupation(1);
+	//mapshow->set_hairstyle(43);
+	//mapshow->set_haircolor(86);
+	//mapshow->set_facestyle(114);
+
+	//mapBase->set_allocated_mapdata(mapdata);
+	//mapBase->set_allocated_mapshow(mapshow);
+	//mapBase->set_charid(70022);
+	//mapBase->set_name(std::string("SanGawku"));
+	//fightBase->set_curfightvalue(0);
+	//charBase->set_type(2);
+
+
+	//charMain->set_allocated_attridata(attrData);
+	//charMain->set_allocated_basedata(charBase);
+	//charMain->set_allocated_fightdata(fightBase);
+	//charMain->set_allocated_mapdata(mapBase);
+	//pktMain->set_allocated_data(charMain);
+	////cutscene->set_onfinish();
+
+	//const unsigned short protobuff_data_size3 = pktMain->ByteSizeLong();
+	//std::vector<BYTE> protobuff_buffer3(protobuff_data_size3);
+	//pktMain->SerializeToArray(protobuff_buffer3.data(), protobuff_data_size3);
+
+
+	//LOG_DEBUG << "Protobuff packet to send HEX: ";
+	//log_data(protobuff_buffer3.data(), protobuff_data_size3);
+
+	//res_test resp3;
+	//MessageBuffer buffer3;
+
+	//buffer3.Resize((sizeof(res_test) + protobuff_data_size3));
+
+	//resp3.size = (sizeof(res_test) - HEADER_SIZE) + protobuff_data_size3;
+	//resp3.CMD = 2261;
+	//resp3.compress = 0;
+	//resp3.encrypt = 0;
+	//resp3.pad = 0x81;//Timestamp
+	//resp3.pad1 = 0xde;
+	//resp3.pad2 = 0x46;
+	//resp3.pad3 = 0xdf;
+	//resp3.protobuff_length = protobuff_data_size3;
+
+
+	//buffer3.Write(&resp3, sizeof(res_test));
+	//buffer3.Write(protobuff_buffer3.data(), protobuff_data_size3);
+	//LOG_DEBUG << "Before Padding Size " << buffer3.GetBufferSize();
+	//// -- padding
+	//padding = 0;
+	//while (((sizeof(res_test) + protobuff_data_size3) + padding) % 8 != 0 && padding < 10)
+	//	buffer3.Write(&++padding, 1);
+
+
+	//log_data(buffer3.GetReadPointer(), sizeof(res_test) + protobuff_data_size3 + 2);
+	//LOG_DEBUG << "Real Packet Size: " << buffer3.GetBufferSize();
+
+	//_writeQueue.push(std::move(buffer3));
+	return true;
+}
+
+bool CharSocket::onSelectCharToLogin(const Packet& packet)
+{
+	
+
+	LOG_DEBUG << "on Select Char to Login";
+	msg::MSG_Req_SelectCharToLogin_CS _hero;
+	fill_my_data(_hero, (unsigned char*)packet.GetPacketData(), packet.GetPacketHeader().size);
+	LOG_DEBUG << _hero.DebugString();
+
+
+
+	//Move to map
+	msg::MSG_Ret_UserMapInfo_SC* mapInfo = new msg::MSG_Ret_UserMapInfo_SC();
+	msg::FloatMovePos* pos = new msg::FloatMovePos();
+	pos->set_fx(801.f);
+	pos->set_fy(908.f);
+	mapInfo->set_mapid(695);
+	mapInfo->set_filename(std::move(std::string("Pc_sgc")));
+	mapInfo->set_mapname(std::move(std::string("Time and space city")));
+	mapInfo->set_lineid(0);
+	mapInfo->set_sceneid(0);
+	mapInfo->set_allocated_pos(pos);
+	mapInfo->set_copymapidx(0);
+	mapInfo->set_subcopymapidx(0);
+
+	const unsigned short protobuff_data_size = mapInfo->ByteSizeLong();
+	std::vector<BYTE> protobuff_buffer(protobuff_data_size);
+	mapInfo->SerializeToArray(protobuff_buffer.data(), protobuff_data_size);
+
+
+	LOG_DEBUG << "Protobuff packet to send HEX: ";
+	log_data(protobuff_buffer.data(), protobuff_data_size);
+
+	res_test resp;
+	MessageBuffer buffer;
+
+	buffer.Resize((sizeof(res_test) + protobuff_data_size));
+
+	resp.size = (sizeof(res_test) - HEADER_SIZE) + protobuff_data_size;
+	resp.CMD = 2273;
+	resp.compress = 0;
+	resp.encrypt = 0;
+	resp.pad = 0x81;
+	resp.pad1 = 0xde;
+	resp.pad2 = 0x46;
+	resp.pad3 = 0xdf;
+	resp.protobuff_length = protobuff_data_size;
+
+
+	buffer.Write(&resp, sizeof(res_test));
+	buffer.Write(protobuff_buffer.data(), protobuff_data_size);
+	LOG_DEBUG << buffer.GetBufferSize();
+	// -- padding
+	BYTE padding = 0;
+	while (((sizeof(res_test) + protobuff_data_size) + padding) % 8 != 0 && padding < 10)
+		buffer.Write(&++padding, 1);
+
+	LOG_DEBUG << "PACKET TO SEND HEX:";
+	log_data(buffer.GetReadPointer(), sizeof(res_test) + protobuff_data_size);
+
+	_writeQueue.push(std::move(buffer));
+
+	//Set Main Char
 	msg::MSG_DataCharacterMain_SC* pktMain = new msg::MSG_DataCharacterMain_SC();
 	msg::CharacterMainData* charMain = new msg::CharacterMainData();
 	msg::AttributeData* attrData = new msg::AttributeData();
@@ -529,46 +803,116 @@ bool CharSocket::onReceiveTeamMemberReq(const Packet& packet)
 	msg::MapUserData* mapBase = new msg::MapUserData;
 	msg::CharacterMapData* mapdata = new msg::CharacterMapData;
 	msg::CharacterMapShow* mapshow = new msg::CharacterMapShow;
-	attrData->set_accurate(100);
-	attrData->set_bang(100);
-	attrData->set_block(100);
-	attrData->set_maxhp(100);
-	attrData->set_maxmp(100);
-	attrData->set_hp(100);
-	attrData->set_mp(100);
+	attrData->set_hp(10);
+	attrData->set_maxhp(10);
+	attrData->set_mp(10);
+	attrData->set_maxmp(10);
+	attrData->set_str(10);
+	attrData->set_dex(10);
+	attrData->set_intel(10);
+	attrData->set_phy(10);
+	attrData->set_matt(10);
+	attrData->set_patt(10);
+	attrData->set_mdef(10);
+	attrData->set_pdef(10);
+	attrData->set_bang(10);
+	attrData->set_bangextradamage(10);
+	attrData->set_toughness(10);
+	attrData->set_toughnessreducedamage(10);
+	attrData->set_penetrate(10);
+	attrData->set_hit(10);
+	attrData->set_penetrateextradamage(10);
+	attrData->set_block(10);
+	attrData->set_blockreducedamage(10);
+	attrData->set_accurate(10);
+	attrData->set_accurateextradamage(10);
+	attrData->set_hold(10);
+	attrData->set_holdreducedamage(10);
+	attrData->set_deflect(10);
+	attrData->set_deflectreducedamage(10);
+	attrData->set_dodge(10);
+	attrData->set_weaponatt(10);
+	attrData->set_firemastery(10);
+	attrData->set_icemastery(10);
+	attrData->set_lightningmastery(10);
+	attrData->set_brightmastery(10);
+	attrData->set_darkmastery(10);
+	attrData->set_fireresist(10);
+	attrData->set_iceresist(10);
+	attrData->set_lightningresist(10);
+	attrData->set_brightresist(10);
+	attrData->set_darkresist(10);
+	attrData->set_firepen(10);
+	attrData->set_icepen(10);
+	attrData->set_lightningpen(10);
+	attrData->set_brightpen(10);
+	attrData->set_darkpen(10);
+	attrData->set_blowint(10);
+	attrData->set_knockint(10);
+	attrData->set_floatint(10);
+	attrData->set_superhitint(10);
+	attrData->set_blowresist(10);
+	attrData->set_knockresist(10);
+	attrData->set_floatresist(10);
+	attrData->set_superhitresist(10);
+	attrData->set_blowdectime(10);
+	attrData->set_knockdectime(10);
+	attrData->set_floatdectime(10);
+	attrData->set_superhitdectime(10);
+	attrData->set_stiffaddtime(10);
+	attrData->set_stiffdectime(10);
+
+
+	charBase->set_exp(1);
+	charBase->set_welpoint(1);
+	charBase->set_money(1);
+	charBase->set_stone(1);
+	charBase->set_tilizhi(1);
+	charBase->set_type(1);
+	charBase->set_famelevel(1);
+	charBase->set_position(0);
+	charBase->set_viplevel(1);
+	charBase->set_port(0);
+	charBase->set_laststage(1);
+	charBase->set_nextexp(1000);
+	charBase->set_pkmode(0);
+	charBase->set_edupoint(1);
+	charBase->set_cooppoint(1);
+	charBase->set_bluecrystal(1);
+	charBase->set_purplecrystal(1);
+	charBase->set_vigourpoint(1);
+	charBase->set_doublepoint(1);
+	charBase->set_bluecrystalincnum(1);
+	charBase->set_purplecrystalincnum(1);
+	charBase->set_familyatt(0);
+	charBase->set_herothisid((std::string)"70022");
 	
-
-	charBase->set_exp(10000);
-	charBase->set_cooppoint(1000);
-	std::string s("Female Saiyan");
-	charBase->set_herothisid(s);
-
-	charBase->set_viplevel(5);
 	mapdata->set_hp(100);
 	mapdata->set_maxhp(100);
 	mapdata->set_level(10);
-	
-	msg::FloatMovePos pos;
-	pos.set_fx(801.0f);
-	pos.set_fy(905.0f);
 
-	mapdata->set_allocated_pos(&pos);
-	mapdata->set_movespeed(30);
+
+	mapdata->set_allocated_pos(pos);
+	mapdata->set_movespeed(10);
 	mapdata->set_country(1);
-	mapdata->set_dir(1);
+	mapshow->set_weapon(0);
+	
+	mapdata->set_dir(0);
 	mapshow->set_heroid(70022);
 	mapshow->set_avatarid(70022);
-	mapshow->set_occupation(1);
+	mapshow->set_occupation(0);
 	mapshow->set_hairstyle(43);
 	mapshow->set_haircolor(86);
-	mapshow->set_facestyle(114);
-
+	mapshow->set_facestyle(49);
+	mapshow->set_bodystyle(0);
+	mapBase->set_charid(_hero.charid());
+	mapBase->set_name(std::string("SanGawku"));
+	
 	mapBase->set_allocated_mapdata(mapdata);
 	mapBase->set_allocated_mapshow(mapshow);
-	mapBase->set_charid(70022);
-	mapBase->set_name(std::string("SanGawku"));
+	mapBase->set_allocated_bakhero(mapshow);
+	
 	fightBase->set_curfightvalue(0);
-	charBase->set_type(2);
 
 
 	charMain->set_allocated_attridata(attrData);
