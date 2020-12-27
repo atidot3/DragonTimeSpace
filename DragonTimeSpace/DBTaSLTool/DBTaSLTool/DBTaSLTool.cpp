dc29dee00134b8780d3f7db625bdbcdbbb434bdd
Sangawku <kalisto2002@gmail.com>
2020-12-27 13:10:00 -0800

    Update the tool, loads TBX files now.

--- DragonTimeSpace/DBTaSLTool/DBTaSLTool/DBTaSLTool.cpp
+++
@@ -1,7 +0,0 @@
-#include "DBTaSLTool.h"
-
-DBTaSLTool::DBTaSLTool(QWidget *parent)
-    : QMainWindow(parent)
-{
-    ui.setupUi(this);
-}
---
+++ DragonTimeSpace/DBTaSLTool/DBTaSLTool/ui_mainwindow.h
@@ -0,0 +1,84 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.15.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionQuit;
+    QAction *actionLoad_TBX;
+    QWidget *centralwidget;
+    QMenuBar *menubar;
+    QMenu *menuFile;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(800, 600);
+        actionQuit = new QAction(MainWindow);
+        actionQuit->setObjectName(QString::fromUtf8("actionQuit"));
+        actionLoad_TBX = new QAction(MainWindow);
+        actionLoad_TBX->setObjectName(QString::fromUtf8("actionLoad_TBX"));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 21));
+        menuFile = new QMenu(menubar);
+        menuFile->setObjectName(QString::fromUtf8("menuFile"));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        menubar->addAction(menuFile->menuAction());
+        menuFile->addAction(actionLoad_TBX);
+        menuFile->addSeparator();
+        menuFile->addAction(actionQuit);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        actionQuit->setText(QCoreApplication::translate("MainWindow", "Quit", nullptr));
+        actionLoad_TBX->setText(QCoreApplication::translate("MainWindow", "Load TBX", nullptr));
+#if QT_CONFIG(tooltip)
+        actionLoad_TBX->setToolTip(QCoreApplication::translate("MainWindow", "Load TBX", nullptr));
+#endif // QT_CONFIG(tooltip)
+        menuFile->setTitle(QCoreApplication::translate("MainWindow", "File", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
